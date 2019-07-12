f66c71d02be511b4c3dc73c1fdf48ecbaa692407
alan-w-255 <1649022341@qq.com>
2019-07-12 11:12:32 +0800

    feat: optimize the product show page

--- src/components/ProductShowBoard.vue
+++ src/components/ProductShowBoard.vue
@@ -4,14 +4,23 @@
       <waterfallSlot
         v-for="item in productList"
         :width="550"
-        :height="400"
-        :key="item.productID"
+        :height="600"
+        :key="item.prono"
       >
-        <Card style="width:100%">
+        <Card style="width:550; height:600;">
           <p slot="title">
-            <Icon type="md-cash" size="24"></Icon>
-            {{item.productName}}
+            <Icon type="md-cash" size="24" color="#ff9900"></Icon>
+            {{item.proname}}
           </p>
+          <div class="product-card">
+           <span class="product-rate">{{item.prorate}}</span> <span style="font-size:10px; color: #808695"> 七日年化</span>
+           <br/>
+           {{item.prodesc}}
+            <br/>
+            金额：{{item.saleamt}} Yuan
+            <br/>
+            <p>失效日期: {{item.invaliddate}}</p>
+          </div>
         </Card>
       </waterfallSlot>
     </Waterfall>
@@ -20,18 +29,22 @@
 <script>
 import Waterfall from "vue-waterfall/lib/waterfall";
 import WaterfallSlot from "vue-waterfall/lib/waterfall-slot";
+import Axios from "axios"
 
 export default {
   data() {
     return {
       productList: [
         {
-          productID: 1,
-          productName: "理财产品 1"
-        },
-        {
-          productID: 2,
-          productName: "理财产品 2"
+          prono: 1,
+          proname: "理财产品 1",
+          prodesc: "4.9",
+          prorate: "",
+          saleamt: "",
+          invaliddate: "",
+          expiredate: "",
+          fsflwno: ""
+
         }
       ]
     };
@@ -40,6 +53,24 @@ export default {
     Waterfall,
     WaterfallSlot
   },
-  methods: {}
+  methods: {},
+  mounted: function () {
+      Axios.get('http://localhost:8099/recommendedProducts')
+        .then(({data}) => {
+            console.log(data)
+            this.productList = data
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+  }
 };
 </script>
+
+<style>
+.product-rate {
+  text-emphasis: em;
+  color: #F9A704;
+  font-size: 20px;
+}
+</style>
